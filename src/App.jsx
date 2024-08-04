60b5b29171443568a525d0b267b05825f57e559d
Tarek Nabil <tareknabil698@gmail.com>
2024-08-04 12:42:10 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
+import FeatureSection from './components/FeatureSection'
+import WorkFlow from './components/WorkFlow'
+import Pricing from './components/Pricing'
+import Testimonials from './components/Testimonials'
+import Footer from './components/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Navbar/>
+    <div className='max-w-7xl mx-auto pt-20 px-6'>
+      <HeroSection/>
+      <FeatureSection/>
+      <WorkFlow/>
+      <Pricing/>
+      <Testimonials/>
+      <Footer/>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
